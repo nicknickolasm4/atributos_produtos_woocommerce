bbc287471810be394f69b5618aa8e52fdaee509c
nicknickolasm4 <nicknickolasm4@gmail.com>
2023-11-14 17:21:58 -0300

    aa

--- custom-product.php
+++ custom-product.php
@@ -1,10 +1,30 @@
 <?php
-/*
-Plugin Name: Product Fields Woocommerce (Nickolas Mariano)
-Description: Adiciona campos personalizados (EAN, NCM, GTIN) aos produtos do WooCommerce.
-Version: 1.2
-Author: Nickolas Mariano
-*/
+/**
+ * Plugin Name:         Product Fields Woocommerce (Nickolas Mariano)
+ * Plugin URI:           https://github.com/nicknickolasm4/atributos_produtos_woocommerce
+ * Description:          Adiciona campos personalizados (EAN, NCM, GTIN) aos produtos do WooCommerce.
+ * Author:               Nickolas Mariano
+ * Author URI:           https://github.com/nicknickolasm4/
+ * Version:              1.0.0
+ * License:              GPLv2 or later
+ * WC requires at least: 3.3.0
+ * WC tested up to:      8.8.1
+ *
+ * This plugin is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * any later version.
+ *
+ * This plugin is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this plugin. If not, see
+ * <https://www.gnu.org/licenses/gpl-2.0.txt>.
+ *
+ */
 
 if (!defined('ABSPATH')) {
     exit;
---
+++ readme.txt
@@ -0,0 +1,36 @@
+=== (EAN, NCM, GTIN) Product Fields ===
+Requires PHP: 7.0
+License: GPLv2 or later
+License URI: https://www.gnu.org/licenses/gpl-2.0.html
+Adiciona campos personalizados (EAN, NCM, GTIN) aos produtos do WooCommerce.
+
+
+=== (EAN, NCM, GTIN) Product Fields ===
+Contributors: nicknickolasm4
+Donate link: https://fernandoacosta.net
+Tags:  woocommerce, ncm, ean, autocomplete, gtin
+Requires at least: 5.0.0
+Tested up to: 6.0
+Stable tag: trunk
+Requires PHP: 7.0.0
+License: GPLv3 or later
+License URI: http://www.gnu.org/licenses/gpl-3.0.html
+
+Adiciona campos personalizados (EAN, NCM, GTIN) aos produtos do WooCommerce.
+
+== Description ==
+
+Este plugin vai Adiciona campos personalizados (EAN, NCM, GTIN) aos produtos do WooCommerce.
+
+Nenhuma configuração é necessária.
+
+= Por que usar esse plugin? =
+
+Esse plugin funciona para adicionar atributos ao seu produto que poderá ser utilizado em planilhas ou em sistemas externos.
+
+
+== Changelog ==
+
+= 1.0.0 - 2023/11/14 =
+
+- Primeira versão :)
